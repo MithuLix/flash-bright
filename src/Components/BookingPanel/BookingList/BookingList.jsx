911e69148f8f43109be6498f6d3535cd5a2cc8eb
mithulix <mohaiminul49@gmail.com>
2021-08-22 07:24:20 +0600

    new

--- src/Components/BookingPanel/BookingList/BookingList.jsx
+++ src/Components/BookingPanel/BookingList/BookingList.jsx
@@ -1,32 +1,73 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { UserContext } from '../../../App';
-import BookingListCards from './BookingListCards';
-import { Link } from 'react-router-dom';
+// import React, { useContext, useEffect, useState } from 'react';
+// import { UserContext } from '../../../App';
+// import BookingListCards from './BookingListCards';
+// import { Link } from 'react-router-dom';
 
-export default function BookingList() {
+// export default function BookingList() {
+//     const [loggedInUser] = useContext(UserContext)
+//     const [bookingLists, setBookingLists] = useState([]);
+//     const [spinner, setSpinner] = useState(true);
+
+//     useEffect(() => {
+//         fetch("https://flash-bright-backend.herokuapp.com/getBookingList?email=" + loggedInUser.email)
+//             .then(res => res.json())
+//             .then(data => { setBookingLists(data); setSpinner(false) })
+//     }, [])
+
+
+//     return (
+//         <div className="container bg-gray-700 rounded p-6 h-auto md:w-2/3 text-center">
+//             <h6 className="text-white text-center text-2xl font-medium pb-6"> Dear, <span className="text-red-400">{loggedInUser.name}</span> You've <span className="text-red-400">{bookingLists.length}</span> Service.</h6>
+//             <div class=" flex justify-center items-center">
+//                 {spinner && <div class="animate-spin rounded-full h-10 w-10 border-b-2 border-white"></div>}
+//             </div>
+//             <div id="bookingList">
+//                 {bookingLists.map(list => <BookingListCards list={list} key={list._id}></BookingListCards>)}
+//             </div>
+//             <Link to={'/services'}>
+//                 <button className=" font-medium bg-gray-700 border hover:bg-gray-600 text-white px-4 py-2 my-4 rounded focus:outline-none">Buy a Service</button>
+//             </Link>
+//         </div>
+//     );
+// };
+
+import React, { useContext, useEffect, useState } from 'react';
+import { Col, Container, Row, Spinner } from 'react-bootstrap';
+import { UserContext } from '../../../App';
+import BookingDrawer from '../BookingDrawer/BookingDrawer';
+import BookingListCards from './BookingListCards';
+
+export default function BookingList () {
     const [loggedInUser] = useContext(UserContext)
     const [bookingLists, setBookingLists] = useState([]);
     const [spinner, setSpinner] = useState(true);
 
     useEffect(() => {
         fetch("https://flash-bright-backend.herokuapp.com/getBookingList?email=" + loggedInUser.email)
-            .then(res => res.json())
-            .then(data => { setBookingLists(data); setSpinner(false) })
+        .then(res => res.json())
+        .then(data => {setBookingLists(data); setSpinner(false)})
     }, [])
 
+    //css
+    const book = { backgroundColor:"#f1f1f1"}
+    const h6 = { margin:"0", padding:"1rem", backgroundColor:"#fff",}
 
     return (
-        <div className="container bg-gray-700 rounded p-6 h-auto md:w-2/3 text-center">
-            <h6 className="text-white text-center text-2xl font-medium pb-6"> Dear, <span className="text-red-400">{loggedInUser.name}</span> You've <span className="text-red-400">{bookingLists.length}</span> Bookings.</h6>
-            <div class=" flex justify-center items-center">
-                {spinner && <div class="animate-spin rounded-full h-10 w-10 border-b-2 border-white"></div>}
-            </div>
-            <div id="bookingList">
-                {bookingLists.map(list => <BookingListCards list={list} key={list._id}></BookingListCards>)}
-            </div>
-            <Link to={'/services'}>
-                <button className=" font-medium bg-gray-700 border hover:bg-gray-600 text-white px-4 py-2 my-4 rounded focus:outline-none">Buy a Service</button>
-            </Link>
+        <div>
+            <Container>
+                <Row className="g-0">
+                    <Col><BookingDrawer/></Col>
+                    <Col md={10}style={book}>
+                        <h6 style={h6}> DEAR, <span className="text-uppercase text-danger">{loggedInUser.name}</span> YOU'VE <span className="text-danger">{bookingLists.length}</span> service.</h6>
+                        <Row className="g-4 mt-3"> 
+                            {spinner && <div className="text-center"> <Spinner animation="border" variant="secondary" /></div>} 
+                        </Row>
+                        <Col id="bookingList">
+                            {bookingLists.map(list => <BookingListCards list={list} key={list._id}></BookingListCards>)}
+                        </Col>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
--- src/Components/BookingPanel/BookingList/BookingListCards.jsx
+++ src/Components/BookingPanel/BookingList/BookingListCards.jsx
@@ -1,19 +1,43 @@
+// import React from 'react';
+
+
+// export default function BookingListCards(props) {
+//     const { title, status } = props.list;
+
+//     return (
+//         <div>
+//             <div>
+//                 <section className="d-flex justify-content-between mt-2">
+//                     <h5>{title}</h5>
+//                     <button type="submit" className="shadow bg-gray-700 hover:bg-gray-600 border focus:shadow-outline focus:outline-none text-white font-bold py-3 px-6 my-2 rounded">
+//                         {status}
+//                     </button>
+//                 </section>
+//             </div>
+//         </div>
+//     );
+// }
+
+
 import React from 'react';
+import { Button, Card, Container } from 'react-bootstrap';
 
 
-export default function BookingListCards(props) {
-    const { title, status } = props.list;
+export default function BookingListCards (props) {
+    const {title, status} = props.list;
+    //css
+    const titleBtn = { textDecoration: 'inherit', color:"white", backgroundColor:"#4f4f4f",border:"none", fontWeight:"500"};
+    const cards  = { lineHeight:"1.2rem", border:"0", background: "rgba(255,255,255,0.8)",
+                     boxShadow: "10px 10px 20px -6px rgba(0,0,0,0.2)"}
 
     return (
-        <div>
-            <div>
-                <section className="d-flex justify-content-between mt-2">
+        <Container>
+            <Card style={cards}>
+                <Card.Body  className="d-flex justify-content-between mt-2">
                     <h5>{title}</h5>
-                    <button type="submit" className="shadow bg-gray-700 hover:bg-gray-600 border focus:shadow-outline focus:outline-none text-white font-bold py-3 px-6 my-2 rounded">
-                        {status}
-                    </button>
-                </section>
-            </div>
-        </div>
+                    <Button style={titleBtn} className="btn-sm">{status}</Button>
+                </Card.Body>
+            </Card>
+        </Container>
     );
 }
